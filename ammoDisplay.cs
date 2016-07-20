899137e2fc2f0879e06f431ee583f4231416ec59
Dirk B <dirk.brouwer58@gmail.com>
2016-07-21 00:05:20 +0200

    Ammo/reload mechanic
    Displays ammo count and reloads when the player is out of ammo.

---
+++ ammoDisplay.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ammoDisplay : MonoBehaviour {
+
+    public static int ammo;
+    Text ammoCount;
+   
+	// Use this for initialization
+	void Start ()
+    {
+
+        ammoCount = GetComponent<Text>();
+        ammo = 10;
+        
+
+    }
+	
+	//Update is called once per frame
+	void Update ()
+    {
+        ammoCount.text = "Ammo: " + ammo;
+    }
+}
