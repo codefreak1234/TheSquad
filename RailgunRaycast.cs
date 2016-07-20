447fe5c11862cd02441062a2672bc2e23c003745
Dirk B <dirk.brouwer58@gmail.com>
2016-07-20 18:40:43 +0200

    Railgun weapon shooting mechanic(Updated)
    Sound files for charging and shooting the gun now working properly

---
+++ RailgunRaycast.cs
@@ -0,0 +1,116 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RailgunRaycast : MonoBehaviour
+{
+    public float timeBetweenBullets;
+    public int damage = 10;
+    public float range = 150;
+    public AudioClip railgunShotSound;
+    public AudioClip railgunChargeSound;
+
+    int shootableMask;
+    float timer;
+    Ray shootRay;
+    private LineRenderer gunLine;
+    RaycastHit hit;
+    private AudioSource railgunAudio;
+    float effectsDisplayTime = 0.2f;
+    ParticleSystem railgunCharge;
+    private bool playing = false;
+    
+   
+
+    // Use this for initialization
+    void Awake()
+    {
+        shootableMask = LayerMask.GetMask("Shootable");
+        gunLine = GetComponent<LineRenderer>();
+        railgunAudio = GetComponent<AudioSource>();
+        railgunCharge = GetComponent<ParticleSystem>();
+        //railgunShotSound = GetComponent<AudioClip>();
+        //railgunChargeSound = GetComponent<AudioClip>();
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.time;
+
+        if (Input.GetButton("Fire1") && timer >= timeBetweenBullets)
+        {
+            if (!playing)
+            {
+                playing = true;
+                railgunAudio.clip = railgunChargeSound;
+                railgunAudio.Stop();
+                railgunAudio.Play();
+            }
+            // Charge() ;
+        }
+        else if (Input.GetButtonUp("Fire1") && playing)
+        {
+            playing = false;
+            railgunAudio.Stop();
+            shoot();
+        }
+
+        //if(Input.GetButton("Fire1") && timer >= timeBetweenBullets)
+        //{
+        //    railgunAudio.clip = railgunChargeSound;
+        //    railgunAudio.Stop();
+        //    railgunAudio.Play();
+        //}
+
+        //else if (Input.GetButtonUp("Fire1"))
+        //{
+        //    railgunAudio.Stop();
+        //    railgunCharge.Stop();
+        //    shoot();
+        //}
+
+        if (timer >= timeBetweenBullets * effectsDisplayTime)
+        {
+            DisableEffects();
+        }
+    }
+
+    public void DisableEffects()
+    {
+        gunLine.enabled = false;
+    }
+
+    public void shoot()
+    {
+        timer = 0f;
+
+        railgunAudio.clip = railgunShotSound;
+        gunLine.enabled = true;
+        
+        railgunAudio.Play();
+        gunLine.SetPosition(0, transform.position);
+
+        shootRay.origin = transform.position;
+        shootRay.direction = transform.right;
+      
+        if (Physics.Raycast(shootRay, out hit, shootableMask))
+        {
+         
+            EnemyHealth dmgscript = hit.collider.gameObject.GetComponent<EnemyHealth>();
+            if(dmgscript != null)
+
+                {
+                    dmgscript.TakeDamage(damage, hit.point);
+                }
+
+                gunLine.SetPosition(1, hit.point);
+        }
+
+        else
+        {
+            gunLine.SetPosition(1, shootRay.origin + shootRay.direction * range);
+        }
+    }
+}
