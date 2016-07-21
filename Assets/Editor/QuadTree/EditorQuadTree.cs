1c61e848544d7da562e1c2b4904ca45cd35ffd60
Zoe Love <zoelove.zle@gmail.com>
2016-07-21 15:26:30 +0530

    Added Editor scripts and map for testing quadTree.Added gizmoes for map and also it querys targets

---
+++ Assets/Editor/QuadTree/EditorQuadTree.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEditor ;
+// using System.Collections;
+
+[CustomEditor(typeof(QuadTree))]
+public class QuadTreeEditor : PropertyDrawer {
+    public override void OnGUI(Rect position, SerializedProperty property, GUIContent label) {
+
+    }
+}
